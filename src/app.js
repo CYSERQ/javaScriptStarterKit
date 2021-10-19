7d12eda2a955d2a6dfac2c1f1095d3e5effe2931
Çağrı Yıldırım <cjcyserq@gmail.com>
2021-10-19 21:39:48 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,34 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun = 9.20
+
+let dolarBugun = 9.30
+
+dolarDun = "9.20"
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11
+console.log(euroDun)
+
+let konutKredileri = ["Konut kredisi","Emlak konut kredisi","kamu konut kredisi"]
+
+console.log("<ul>")
+for(let i = 0; i<3;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+
+{/* <ul>
+    <li>konut kredileri</li>
+    <li>kamu kredileri</li>
+</ul> */}
+
+
+console.log(konutKredileri)
