69b74e4d86e1a33e46ed37de6156ff595e42b851
CYSERQ <cjcyserq@gmail.com>
2021-11-15 19:00:26 +0300

    last ders2

---
+++ src/mapfilterreduce.js
@@ -0,0 +1,9 @@
+let cart = [
+    {id:1, productName:"telefon", quantity:3, unitPrice:4000}
+    {id:2, productName:"bardak", quantity:2, unitPrice:30}
+    {id:3, productName:"kalem", quantity:1, unitPrice:20}
+    {id:4, productName:"şarj cihazı", quantity:2, unitPrice:100}
+    {id:5, productName:"kitap", quantity:3, unitPrice:30}
+    {id:6, productName:"pot", quantity:5, unitPrice:150}  
+]
+
